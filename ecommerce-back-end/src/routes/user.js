18e02518529aa77d6810c279a3b1d3e439daf8f8
Bhavya <bhavyakrk@gmail.com>
2021-01-09 15:53:52 +0530

    Added user controller

--- ecommerce-back-end/src/routes/user.js
+++ ecommerce-back-end/src/routes/user.js
@@ -1,6 +1,7 @@
 const express=require('express');
+const { signup } = require('../controller/user');
 const router= express.Router();
-const User=require('../models/user');
+
 
 
 router.post('/signin',(req,res)=>{
@@ -9,53 +10,7 @@ router.post('/signin',(req,res)=>{
 
 });
 
-router.post('/signup',(req,res)=>{
-
-
-    User.findOne({email:req.body.email})
-    .exec((error,user)=>{
-        if(user)return res.status(400).json({
-            message:'User already registered'
-        });
-        const {
-            firstName,
-            lastName,
-            email,
-            password
-        }=req.body;
-        const _user=new User({
-            firstName,
-            lastName,
-            email,
-            password,
-
-            username:Math.random().toString()
-
-
-        });
-
-        _user.save((error,data)=>{
-            if(error){
-
-                return res.status(400).json({
-                    message:'Somathing went wrong'
-
-                });
-            }
-            if(data){
-
-                return res.status(201).json({
-                    user:data
-                })
-            }   
-
-        });
-
-    });
-
-
-
-});
+router.post('/signup',signup);
 
 
 
