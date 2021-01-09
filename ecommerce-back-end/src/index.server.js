2e8c3ab58d6c8010e8f6b3d6d26e9423b610d1c2
Bhavya <bhavyakrk@gmail.com>
2021-01-09 12:10:34 +0530

    Express Server check

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -1,7 +1,25 @@
 const express =require('express')
-
+const env=require('dotenv')
 const app=express();
+
+
+
+env.config();
+
+app.get('/',(req,res,next)=>{
+
+
+    res.status(200).json({
+
+        message:'Hello from server'
+    });
+
+    
+
+
+});
 app.listen(process.env.PORT,()=>{
+     
 
     console.log(`Server is running on port ${process.env.PORT}`);
 });
