c34461a6f2fac64d7c7bab1bee044d05f72212e0
Bhavya <bhavyakrk@gmail.com>
2021-01-09 12:38:31 +0530

    Added body parser

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -1,10 +1,17 @@
 const express =require('express')
 const env=require('dotenv')
 const app=express();
+const bodyParser=require('body-parser'); 
 
 
 
 env.config();
+
+//mondodb connection
+//mongodb+srv://root:<password>@cluster0.ygi8r.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+app.use(express.json());
+
 
 app.get('/',(req,res,next)=>{
 
@@ -12,6 +19,20 @@ app.get('/',(req,res,next)=>{
     res.status(200).json({
 
         message:'Hello from server'
+    });
+
+    
+
+
+});
+
+
+app.post('/data',(req,res,next)=>{
+
+
+    res.status(200).json({
+
+        message:req.body
     });
 
     
