91cd788aeb23bd6e7fb93746e548a561c8034764
Bhavya <bhavyakrk@gmail.com>
2021-01-10 15:49:56 +0530

    Admin Portal Creation started[Navbar added]

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import './App.css';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <h1>
+          Hello World
+        </h1>
+      </Layout>
+
+     
+    </div>
+  );
+}
 
 export default App;
