dce86dc559f223716c1ed01d69d0bbb0d92cb0e9
Bhavya <bhavyakrk@gmail.com>
2021-01-11 21:57:00 +0530

    cart api items addition working

--- ecommerce-back-end/src/controller/cart.js
+++ ecommerce-back-end/src/controller/cart.js
@@ -1,20 +1,103 @@
-const Cart=require('../models/cart');
+const Cart = require('../models/cart');
 
 
 
-exports.addItemToCart=(req,res)=>{
+exports.addItemToCart = (req, res) => {
+  Cart.findOne({ user: req.user._id })
+    .exec((error, cart) => {
+      if (error) return res.status(400).json({ error });
+      if (cart) {
+        const product=req.body.cartItems.product;
+        
+        //if cart already exist then update cart.
+        const item=cart.cartItems.find(c=>c.product==product);
+        if(item){
+          Cart.findOneAndUpdate({user:req.user._id,"cartItems.product":product},{
+            "$set":{
+                "cartItems":{
+                  ...req.body.cartItems,
+                  quantity:item.quantity+req.body.cartItems.quantity
+                }
+            }
+  
+          })
+          .exec((error,_cart)=>{
+            if (error) return res.status(400).json({ error });
+            if(_cart){
+              return res.status(201).json({ cart:_cart });
+  
+  
+            }
+  
+  
+  
+  
+          });
 
-  const cart=new Cart({
-      user:req.user._id,
-      cartItems:req.body.cartItems
 
-  });
-  cart.save((error,cart)=>{
-    if(error)return res.status(400).json({error});
-  });
-  if(cart){
-    return res.status(201).json({cart});
-  }
+
+
+
+
+
+        }
+        else{
+
+
+          Cart.findOneAndUpdate({user:req.user._id},{
+            "$push":{
+                "cartItems":req.body.cartItems
+            }
+  
+          })
+          .exec((error,_cart)=>{
+            if (error) return res.status(400).json({ error });
+            if(_cart){
+              return res.status(201).json({ cart:_cart });
+  
+  
+            }
+  
+  
+  
+  
+          });
+          
+        }
+
+
+        
+
+
+
+
+
+        
+
+      }
+      else {
+        // if cart not exist the create one
+        const cart = new Cart({
+
+          user: req.user._id,
+          cartItems: [req.body.cartItems]
+
+        });
+        cart.save((error, cart) => {
+          if (error) return res.status(400).json({ error });
+        });
+        if (cart) {
+          return res.status(201).json({ cart });
+        }
+
+      }
+
+
+
+
+
+    });
+
 
 
 
