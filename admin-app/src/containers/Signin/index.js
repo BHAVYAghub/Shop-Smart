516eeafa18acc367e3c1bc0a50d8e69d3448743b
Bhavya <bhavyakrk@gmail.com>
2021-01-12 12:58:01 +0530

    Added redux store

--- admin-app/src/containers/Signin/index.js
+++ admin-app/src/containers/Signin/index.js
@@ -1,49 +1,65 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import {Container,Form,Row,Col,Button} from 'react-bootstrap'
+import { Container, Form, Row, Col, Button } from 'react-bootstrap'
 import Input from '../../components/UI/Input'
+import {login} from '../../actions'; 
+import { useDispatch } from 'react-redux';
+
+
 /**
 * @author
 * @function Signin
 **/
 
 const Signin = (props) => {
-  return(
-    <Layout>
+    const dispatch=useDispatch();
 
-        <Container>
-            <Row style={{marginTop:'50px'}}>
-                <Col md={{span: 6,offset:3}}>
-                <Form>
-                <Input
-                                        label="Email"
-                                        placeholder="Email"
-                                        value=""
-                                        type="email"
-                                         onChange={()=>{}}
-                                    />
 
-                        <Input
-                                        label="Password"
-                                        placeholder="Password"
-                                        value=""
-                                        type="password"
-                                        onChange={()=>{}}
-                                    />
-                
-                <Button variant="primary" type="submit">
-                    Submit
+    const userLogin=(e)=>{
+        e.preventDefault();
+        const user={
+            email:'bhavyaad1@gmail.com',
+            password:'123456'
+
+        }
+        dispatch(login(user));
+    }
+    return (
+        <Layout>
+
+            <Container>
+                <Row style={{ marginTop: '50px' }}>
+                    <Col md={{ span: 6, offset: 3 }}>
+                        <Form onSubmit={userLogin}>
+                            <Input
+                                label="Email"
+                                placeholder="Email"
+                                value=""
+                                type="email"
+                                onChange={() => { }}
+                            />
+
+                            <Input
+                                label="Password"
+                                placeholder="Password"
+                                value=""
+                                type="password"
+                                onChange={() => { }}
+                            />
+
+                            <Button variant="primary" type="submit">
+                                Submit
                 </Button>
-                </Form>
+                        </Form>
 
-                </Col>
-            </Row>
-        
+                    </Col>
+                </Row>
 
-        </Container>
-    </Layout>
-   )
 
- }
+            </Container>
+        </Layout>
+    )
+
+}
 
 export default Signin
---
+++ admin-app/src/reducers/auth.reducers.js
@@ -0,0 +1,31 @@
+import { authConstants } from "../actions/constants";
+
+const initState={
+    name:'Bhavya'
+
+
+
+};
+
+
+
+export default (state=initState,action)=>{
+    console.log(action);
+    switch(action.type){
+
+        case authConstants.LOGIN_REQUEST:
+            state={
+                ...state,
+                ...action.payload
+
+            }
+            break;
+
+
+    }
+    return state;
+
+
+
+
+}
