a98f73d5dbcfdf4040b468bd94fa083d55d800de
Bhavya <bhavyakrk@gmail.com>
2021-01-12 13:39:39 +0530

    axios for api integration added

--- admin-app/src/actions/auth.actions.js
+++ admin-app/src/actions/auth.actions.js
@@ -1,11 +1,13 @@
+import axios from "../helpers/axios";
 import { authConstants } from "./constants"
+//export is default so we don't have to write exact name of the export
 
 export const login=(user)=>{
     console.log(user);
 
     return async(dispatch)=>{
 
-
+        const res=await axios.post('/admin/signin')
         dispatch({
             type:authConstants.LOGIN_REQUEST,
             payload:{
